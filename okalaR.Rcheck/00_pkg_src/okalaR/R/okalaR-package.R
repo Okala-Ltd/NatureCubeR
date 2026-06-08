6740623c27b8440238286186fe21cc4c1d23522f
AdamVarley30 <adamvarley1988@gmail.com>
2026-06-08 08:47:34 +0100

    Remove okalaR package files and documentation in preparation for deprecation

--- okalaR.Rcheck/00_pkg_src/okalaR/R/okalaR-package.R
+++
@@ -1,7 +0,0 @@
-#' okalaR: API wrapper for the Okala dashboard
-#'
-#' Provides functions to interact with the Okala dashboard API, including project, station, media, and label management.
-#'
-#' @docType package
-#' @name okalaR
-NULL
